03f122cb09125bbff0dbcf9be1440b39662bac6f
Evan McCullough <webmaster@evanamccullough.com>
2014-11-05 21:19:44 -0500

    added new photos" "

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,12 +10,18 @@ class User < ActiveRecord::Base
 
 
   def setphoto
-	if self.email.start_with?('a','b','c','d','e','f','g','h','i','j','k','l','m','n','1','2','3','4','5')
-		@userphoto = "stormtrooper.jpg"
-	else
-		@userphoto = "c3p0.jpg"
-	end
+  	if self.email.start_with?('a','b','c','d','e','1','2')
+  		@userphoto = "gc.jpg"
+    elsif self.email.start_with?('f','g','h','i','j','3','4')
+      @userphoto = "oc.jpg"
+    elsif self.email.start_with?('k','l','m','n','o','5','6')
+      @userphoto = "pc.jpg"
+    elsif self.email.start_with?('p','q','r','s','t','7','8')
+      @userphoto = "plc.jpg"
+    elsif self.email.start_with?('u','v','w','x','y','z','9','0')
+      @userphoto = "tc.jpg"
+  	end
 
-	self.attributes = {userphoto: @userphoto}
+  	self.attributes = {userphoto: @userphoto}
   end
 end
